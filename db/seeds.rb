0ff4e2862819e2454a608ccfa853e08cb5004808
alexandrechupin <alexandrechupin@gmail.com>
2019-08-13 16:44:48 +0200

    update 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }] )
 #   Character.create(name: 'Luke', movie: movies.first)
+  require 'faker'
+
+  10.times do
+    article = Article.new(
+      title:    Faker::Lorem.word,
+      content: Faker::Lorem.sentences(number: 1)
+    )
+    article.save!
+  end
